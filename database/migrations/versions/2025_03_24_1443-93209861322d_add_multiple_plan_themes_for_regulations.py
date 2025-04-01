38a1e1ebca96106b904bd59ad43bc35802d0b558
Riku Oja <Rikuoja@users.noreply.github.com>
2025-04-01 13:38:37 +0300

    Bump down revision

--- database/migrations/versions/2025_03_24_1443-93209861322d_add_multiple_plan_themes_for_regulations.py
+++ database/migrations/versions/2025_03_24_1443-93209861322d_add_multiple_plan_themes_for_regulations.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "93209861322d"
-down_revision: Union[str, None] = "82a732dbaebe"
+down_revision: Union[str, None] = "2010b7bcc3b4"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
