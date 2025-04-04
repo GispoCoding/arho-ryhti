99a146be819beff3d5099dbb64e0ae8f78fb957f
Riku Oja <riku.oja@kapsi.fi>
2025-04-04 17:43:10 +0300

    Re-revert back to non-streaming upload

--- test/test_ryhti_client.py
+++ test/test_ryhti_client.py
@@ -137,18 +137,12 @@ def mock_xroad_ryhti_fileupload(requests_mock) -> None:
     def match_request_body(request: PreparedRequest):
         # Check that the file is uploaded:
         assert "multipart/form-data; boundary=" in request.headers["Content-Type"]
-        # Looks like we cannot match streamed content. Trying to match mock request content here results in
-        # some kind of race condition or lock on reading from the content, probably mock-requests does not
-        # expect us to empty the streaming body by reading from it, so it gets stuck on reading the request.
-        # So let's just check that the request has the correct metadata. For streaming requests,
-        # multipartencoder is saved as request body.
-        body: MultipartEncoder = request.body
         return (
-            body.fields["file"][0] == "dummy.pdf"
-            and body.fields["file"][2] == "application/pdf"
+            b'Content-Disposition: form-data; name="file"; filename="GeogToWGS84GeoKey5.tif"'
+            in cast(bytes, request.body)
         ) or (
-            body.fields["file"][0] == "GeogToWGS84GeoKey5.tif"
-            and body.fields["file"][2] == "image/tiff"
+            b'Content-Disposition: form-data; name="file"; filename="dummy.pdf"'
+            in cast(bytes, request.body)
         )
 
     requests_mock.post(
