65983dcb3bb6b9276ae64e596ccb1f6e7673aaaf
Riku Oja <riku.oja@kapsi.fi>
2025-05-02 13:30:15 +0300

    Update postgres version

--- infra/variables.tf
+++ infra/variables.tf
@@ -89,9 +89,9 @@ variable "db_instance_type" {
 }
 
 variable "db_postgres_version" {
-  description = "Version number of the PostgreSQL DB. Default: 13.15"
+  description = "Version number of the PostgreSQL DB. Default: 13.16"
   type        = string
-  default     = "13.15"
+  default     = "13.16"
 }
 
 variable "hame_db_name" {
