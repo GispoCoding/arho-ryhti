d768963aacaf56671ab6c23ea18f39e444ed0f2c
Lauri Kajan <lauri.kajan@gispo.fi>
2025-08-05 17:42:41 +0300

    Typing fixes

--- lambdas/ryhti_client/ryhti_client.py
+++ lambdas/ryhti_client/ryhti_client.py
@@ -1889,7 +1889,14 @@ class RyhtiClient:
                 ):
                     session.add(document)
                     if document_response["status"] == 201:
-                        document.exported_file_key = document_response["detail"]
+                        try:
+                            file_key = UUID(document_response["detail"])
+                        except ValueError:
+                            LOGGER.error(
+                                f"Could not parse file key {document_response['detail']} for document {document.id}!"  # noqa
+                            )
+                            continue
+                        document.exported_file_key = file_key
                         document.exported_at = datetime.datetime.now(tz=LOCAL_TZ)
                         # Save the etag of the uploaded file, piggybacked in response
                         if document_response["warnings"]:
