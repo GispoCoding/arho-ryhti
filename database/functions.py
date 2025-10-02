4e2154f1d54a0d4b44352997603a214544c56308
Lauri Kajan <lauri.kajan@gispo.fi>
2025-10-02 09:23:54 +0300

    Optimize sql

--- database/functions.py
+++ database/functions.py
@@ -69,71 +69,80 @@ primary_use_regulations = PGFunction(
     signature="primary_use_regulations(land_use_area_id uuid)",
     definition=dedent(
         """\
-        RETURNS jsonb
-        STABLE
-        PARALLEL SAFE
-        LANGUAGE sql
+            RETURNS jsonb
+            STABLE
+            PARALLEL SAFE
+            LANGUAGE sql
         AS
         $$
-        WITH additional_info_grouped_by_type AS (
-            SELECT
-                r.id AS regulation_id,
-                tai.value AS type_name,
-                jsonb_agg(
-                    jsonb_strip_nulls(
-                        jsonb_build_object(
-                            'numeric_value', ai.numeric_value,
-                            'unit', ai.unit,
-                            'numeric_range_min', ai.numeric_range_min,
-                            'numeric_range_max', ai.numeric_range_max,
-                            'text_value', ai.text_value,
-                            'text_syntax', ai.text_syntax,
-                            'code_title', ai.code_title,
-                            'code_list', ai.code_list,
-                            'code_value', ai.code_value
-                        )
+            select
+                jsonb_object_agg(
+                    tpr.value,
+                    coalesce(
+                        (
+                            select
+                            jsonb_object_agg(
+                                ai_type,
+                                ai_values_array
+                            )
+                            from (
+                                select
+                                    tai.value ai_type,
+                                    jsonb_agg(
+                                        (
+                                            select
+                                                jsonb_strip_nulls(to_jsonb(ai_values))
+                                            from
+                                                (
+                                                select
+                                                    ai.numeric_value,
+                                                    ai.unit,
+                                                    ai.numeric_range_min,
+                                                    ai.numeric_range_max,
+                                                    ai.text_value,
+                                                    ai.text_syntax,
+                                                    ai.code_title,
+                                                    ai.code_list,
+                                                    ai.code_value
+                                                ) as ai_values
+                                        )
+                                    ) ai_values_array
+                                from
+                                    hame.additional_information ai
+                                    join codes.type_of_additional_information tai
+                                        on ai.type_additional_information_id = tai.id
+                                where
+                                    ai.plan_regulation_id = r.id
+                                    AND tai.value != 'paakayttotarkoitus'
+                                group by tai.value
+                            ) ai_values
+                        ),
+                        '{}'::jsonb
                     )
-                ) AS additional_info_array
-            FROM hame.additional_information ai
-            JOIN codes.type_of_additional_information tai
-                ON ai.type_additional_information_id = tai.id
-            JOIN hame.plan_regulation r
-                ON ai.plan_regulation_id = r.id
-            WHERE tai.value != 'paakayttotarkoitus'
-            GROUP BY r.id, tai.value
-        ),
-        additional_info_per_regulation AS (
-            SELECT
-                regulation_id,
-                jsonb_object_agg(type_name, additional_info_array) AS additional_info_json
-            FROM additional_info_grouped_by_type
-            GROUP BY regulation_id
-        )
-        SELECT
-            jsonb_object_agg(
-                tpr.value,
-                coalesce(additional_info_per_regulation.additional_info_json, '{}'::jsonb)
-            )
-        FROM hame.regulation_group_association rga
-        JOIN hame.plan_regulation_group rg
-            ON rga.plan_regulation_group_id = rg.id
-        JOIN hame.plan_regulation r
-            ON rg.id = r.plan_regulation_group_id
-        JOIN codes.type_of_plan_regulation tpr
-            ON r.type_of_plan_regulation_id = tpr.id
-        LEFT JOIN additional_info_per_regulation
-            ON r.id = additional_info_per_regulation.regulation_id
-        WHERE rga.land_use_area_id = $1
-          AND EXISTS (
-              SELECT 1
-              FROM hame.additional_information ai
-              JOIN codes.type_of_additional_information tai
-                  ON ai.type_additional_information_id = tai.id
-              WHERE ai.plan_regulation_id = r.id
-                AND tai.value = 'paakayttotarkoitus'
-          );
-        $$;
-        """
+                )
+            from
+                hame.regulation_group_association rga
+                join hame.plan_regulation_group rg
+                    on rga.plan_regulation_group_id = rg.id
+                join hame.plan_regulation r
+                    on rg.id = r.plan_regulation_group_id
+                join codes.type_of_plan_regulation tpr
+                    on r.type_of_plan_regulation_id = tpr.id
+            where
+                rga.land_use_area_id = $1
+                AND EXISTS (  -- select only regulations that have paakayttotarkoitus additional information
+                    select
+                    from hame.additional_information ai
+                    where
+                    ai.plan_regulation_id = r.id
+                    AND ai.type_additional_information_id = (
+                        select id
+                        from codes.type_of_additional_information
+                        where value = 'paakayttotarkoitus')
+                )
+        $$
+        ;
+        """  # noqa: E501
     ),
 )
 
@@ -143,71 +152,79 @@ sub_area_regulations = PGFunction(
     signature="sub_area_regulations(other_area_id uuid)",
     definition=dedent(
         """\
-        RETURNS jsonb
-        STABLE
-        PARALLEL SAFE
-        LANGUAGE sql
+            RETURNS jsonb
+            STABLE
+            PARALLEL SAFE
+            LANGUAGE sql
         AS
         $$
-        WITH additional_info_grouped_by_type AS (
-            SELECT
-                r.id AS regulation_id,
-                tai.value AS type_name,
-                jsonb_agg(
-                    jsonb_strip_nulls(
-                        jsonb_build_object(
-                            'numeric_value', ai.numeric_value,
-                            'unit', ai.unit,
-                            'numeric_range_min', ai.numeric_range_min,
-                            'numeric_range_max', ai.numeric_range_max,
-                            'text_value', ai.text_value,
-                            'text_syntax', ai.text_syntax,
-                            'code_title', ai.code_title,
-                            'code_list', ai.code_list,
-                            'code_value', ai.code_value
-                        )
+            select
+                jsonb_object_agg(
+                    tpr.value,
+                    coalesce(
+                        (
+                            select
+                                jsonb_object_agg(
+                                    ai_type,
+                                    ai_values_array
+                                )
+                            from (
+                                select
+                                    tai.value ai_type,
+                                    jsonb_agg(
+                                        (
+                                            select
+                                                jsonb_strip_nulls(to_jsonb(ai_values))
+                                            from (
+                                                select
+                                                        ai.numeric_value,
+                                                        ai.unit,
+                                                        ai.numeric_range_min,
+                                                        ai.numeric_range_max,
+                                                        ai.text_value,
+                                                        ai.text_syntax,
+                                                        ai.code_title,
+                                                        ai.code_list,
+                                                        ai.code_value
+                                            ) as ai_values
+                                        )
+                                    ) as ai_values_array
+                                from
+                                    hame.additional_information ai
+                                    join codes.type_of_additional_information tai
+                                        on ai.type_additional_information_id = tai.id
+                                where
+                                    ai.plan_regulation_id = r.id
+                                    AND tai.value != 'osaAlue'
+                                group by tai.value
+                            ) ai_values
+                        ),
+                        '{}'::jsonb
                     )
-                ) AS additional_info_array
-            FROM hame.additional_information ai
-            JOIN codes.type_of_additional_information tai
-                ON ai.type_additional_information_id = tai.id
-            JOIN hame.plan_regulation r
-                ON ai.plan_regulation_id = r.id
-            WHERE tai.value != 'osaAlue'
-            GROUP BY r.id, tai.value
-        ),
-        additional_info_per_regulation AS (
-            SELECT
-                regulation_id,
-                jsonb_object_agg(type_name, additional_info_array) AS additional_info_json
-            FROM additional_info_grouped_by_type
-            GROUP BY regulation_id
-        )
-        SELECT
-            jsonb_object_agg(
-                tpr.value,
-                coalesce(additional_info_per_regulation.additional_info_json, '{}'::jsonb)
-            )
-        FROM hame.regulation_group_association rga
-        JOIN hame.plan_regulation_group rg
-            ON rga.plan_regulation_group_id = rg.id
-        JOIN hame.plan_regulation r
-            ON rg.id = r.plan_regulation_group_id
-        JOIN codes.type_of_plan_regulation tpr
-            ON r.type_of_plan_regulation_id = tpr.id
-        LEFT JOIN additional_info_per_regulation
-            ON r.id = additional_info_per_regulation.regulation_id
-        WHERE rga.other_area_id = $1
-          AND EXISTS (
-              SELECT 1
-              FROM hame.additional_information ai
-              JOIN codes.type_of_additional_information tai
-                  ON ai.type_additional_information_id = tai.id
-              WHERE ai.plan_regulation_id = r.id
-                AND tai.value = 'osaAlue'
-          );
-        $$;
-        """
+                )
+            from
+                hame.regulation_group_association rga
+                join hame.plan_regulation_group rg
+                    on rga.plan_regulation_group_id = rg.id
+                join hame.plan_regulation r
+                    on rg.id = r.plan_regulation_group_id
+                join codes.type_of_plan_regulation tpr
+                    on r.type_of_plan_regulation_id = tpr.id
+            where
+                rga.other_area_id = $1
+                AND EXISTS (  -- select only regulations that have osaAlue additional information
+                    select
+                    from hame.additional_information ai
+                    where
+                        ai.plan_regulation_id = r.id
+                        AND ai.type_additional_information_id = (
+                            select id
+                            from codes.type_of_additional_information
+                            where value = 'osaAlue')
+                )
+        $$
+        ;
+        """  # noqa: E501
     ),
 )
 
@@ -257,10 +274,10 @@ type_regulations = PGFunction(
     signature="type_regulations(table_name text, id uuid)",
     definition=dedent(
         """\
-        RETURNS jsonb
-        STABLE
-        PARALLEL SAFE
-        LANGUAGE plpgsql
+            RETURNS jsonb
+            STABLE
+            PARALLEL SAFE
+            LANGUAGE plpgsql
         AS
         $BODY$
         DECLARE
@@ -268,55 +285,61 @@ type_regulations = PGFunction(
         BEGIN
             EXECUTE format(
                 $SQL$
-                WITH additional_info_grouped_by_type AS (
-                    SELECT
-                        r.id AS regulation_id,
-                        tai.value AS type_name,
-                        jsonb_agg(
-                            jsonb_strip_nulls(
-                                jsonb_build_object(
-                                    'numeric_value', ai.numeric_value,
-                                    'unit', ai.unit,
-                                    'numeric_range_min', ai.numeric_range_min,
-                                    'numeric_range_max', ai.numeric_range_max,
-                                    'text_value', ai.text_value,
-                                    'text_syntax', ai.text_syntax,
-                                    'code_title', ai.code_title,
-                                    'code_list', ai.code_list,
-                                    'code_value', ai.code_value
-                                )
-                            )
-                        ) AS additional_info_array
-                    FROM hame.additional_information ai
-                    JOIN codes.type_of_additional_information tai
-                        ON ai.type_additional_information_id = tai.id
-                    JOIN hame.plan_regulation r
-                        ON ai.plan_regulation_id = r.id
-                    GROUP BY r.id, tai.value
-                ),
-                additional_info_per_regulation AS (
-                    SELECT
-                        regulation_id,
-                        jsonb_object_agg(type_name, additional_info_array) AS additional_info_json
-                    FROM additional_info_grouped_by_type
-                    GROUP BY regulation_id
-                )
-                SELECT
+                select
                     jsonb_object_agg(
                         tpr.value,
-                        coalesce(additional_info_per_regulation.additional_info_json, '{}'::jsonb)
+                        coalesce(
+                            (
+                                select
+                                    jsonb_object_agg(
+                                        ai_type,
+                                        ai_values_array
+                                    )
+                                from (
+                                    select
+                                        tai.value ai_type,
+                                        jsonb_agg(
+                                            (
+                                                select
+                                                jsonb_strip_nulls(to_jsonb(ai_values))
+                                                from
+                                                    (
+                                                    select
+                                                        ai.numeric_value,
+                                                        ai.unit,
+                                                        ai.numeric_range_min,
+                                                        ai.numeric_range_max,
+                                                        ai.text_value,
+                                                        ai.text_syntax,
+                                                        ai.code_title,
+                                                        ai.code_list,
+                                                        ai.code_value
+                                                    ) as ai_values
+                                            )
+                                        ) as ai_values_array
+                                    from
+                                        hame.additional_information ai
+                                        join codes.type_of_additional_information tai
+                                            on ai.type_additional_information_id = tai.id
+                                    where
+                                        ai.plan_regulation_id = r.id
+                                    group by tai.value
+                                ) ai_values
+                            ),
+                            '{}'::jsonb
+                        )
                     )
-                FROM hame.regulation_group_association rga
-                JOIN hame.plan_regulation_group rg
-                    ON rga.plan_regulation_group_id = rg.id
-                JOIN hame.plan_regulation r
-                    ON rg.id = r.plan_regulation_group_id
-                JOIN codes.type_of_plan_regulation tpr
-                    ON r.type_of_plan_regulation_id = tpr.id
-                LEFT JOIN additional_info_per_regulation
-                    ON r.id = additional_info_per_regulation.regulation_id
-                WHERE rga.%I = $1
-                  AND r.value_data_type IS NULL
+                from
+                    hame.regulation_group_association rga
+                    join hame.plan_regulation_group rg
+                        on rga.plan_regulation_group_id = rg.id
+                    join hame.plan_regulation r
+                        on rg.id = r.plan_regulation_group_id
+                    join codes.type_of_plan_regulation tpr
+                        on r.type_of_plan_regulation_id = tpr.id
+                where
+                    rga.%I = $1
+                    AND r.value_data_type is null
                 $SQL$,
                 table_name||'_id'
             )
@@ -325,11 +348,11 @@ type_regulations = PGFunction(
 
             RETURN return_value;
         END;
-        $BODY$;
+        $BODY$
+        ;
         """
     ),
 )
-
 
 functions = [
     regulation_values,
